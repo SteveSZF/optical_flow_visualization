955878d97736cb1561c7fc0b6295de3f06fec641
SteveSZF <1047629843@qq.com>
2019-07-16 19:46:39 +0800

    add gif

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python visual_flow.py --caffemodel ./FlowNet2_weights.caffemodel.h5 --deployproto ./FlowNet2_camera_deploy.prototxt --video /home/szf/Videos/test2.mp4 --gpu 0 --step 4
+python visual_flow.py --caffemodel ./FlowNet2_weights.caffemodel.h5 --deployproto ./FlowNet2_camera_deploy.prototxt --video /home/szf/Videos/COCFIGHT1/test.mp4 --gpu 0 --step 4
